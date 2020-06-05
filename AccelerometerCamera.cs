0feb5bd3902557a404d8e0dca4dbf544d2dbf2d6
Dean Palma Ivatchkovitch <deankovitch@gmail.com>
2020-06-05 20:23:19 -0300

    Gyro and Accel camera controllers

---
+++ AccelerometerCamera.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using AHRS;
+
+public class AccelerometerCamera : MonoBehaviour
+{
+	private Vector3 flipingVector = Vector3.one;
+	private Vector3 offsetVector = new Vector3(180f, 90f, 90f);
+
+	private Quaternion flippingQuat = Quaternion.identity;
+	private Quaternion offsetQuat = Quaternion.identity;
+
+	public float rotationSpeed = 10.0f;
+	static AHRS.MadgwickAHRS AHRS = new AHRS.MadgwickAHRS(1f / 20f, 0.975f);
+
+	public bool isActive = false;
+
+
+	void Update()
+	{
+		if (isActive == false){
+			return;
+		}
+
+		Vector3 a = Input.acceleration;
+		Vector3 g = Input.gyro.rotationRateUnbiased;
+
+		AHRS.Update(g.x, g.y, g.z, a.x, a.y, a.z);
+		Quaternion quat = new Quaternion(AHRS.Quaternion[2], -AHRS.Quaternion[1], AHRS.Quaternion[0], AHRS.Quaternion[3]);
+
+		quat.eulerAngles = new Vector3(quat.eulerAngles.x * flipingVector.x, quat.eulerAngles.y * flipingVector.y, quat.eulerAngles.z * flipingVector.z );
+		quat *= Quaternion.Euler(offsetVector);
+
+		Quaternion inverseQuat = Quaternion.Inverse(quat);
+		transform.rotation = Quaternion.Slerp(transform.rotation, inverseQuat , rotationSpeed*Time.deltaTime);
+	}
+
+	static float deg2rad(float degrees)
+	{
+		return (float)(Math.PI / 180.0f) * degrees;
+	}
+
+	static float rad2deg(float radians)
+	{
+		return (float)(1.0f / Math.PI) * radians;
+	}
+
+	private float AccelerometerUpdateInterval = 1.0f / 100.0f;
+	private float LowPassKernelWidthInSeconds = 0.001f;
+	private Vector3 lowPassValue = Vector3.zero;
+	Vector3 lowpass()
+	{
+		float LowPassFilterFactor = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;
+		lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);
+		return lowPassValue;
+	}
+}
---
+++ GyroCamera.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCamera : MonoBehaviour
+{
+	private float m_initialYAngle = 0f;
+	private float m_appliedGyroYAngle = 0f;
+	private float m_calibrationYAngle = 0f;
+	[SerializeField] private Transform m_rawGyroRotation;
+	private float m_tempSmoothing;
+
+	[SerializeField] private Transform m_swipeObjectContainer;
+    public Transform SwipeObjectContainer { get { return m_swipeObjectContainer; } }
+
+	[SerializeField] private float m_smoothing = 0.1f;
+
+	public bool isActive = false;
+
+	private IEnumerator Start()
+	{
+		m_initialYAngle = transform.eulerAngles.y;
+        m_rawGyroRotation.position = transform.position;
+        m_rawGyroRotation.rotation = transform.rotation;
+
+        yield return new WaitForSeconds(1);
+		StartCoroutine(CalibrateYAngle());
+	}
+
+	private void Update()
+	{
+		if (isActive == false)
+		{
+			return;
+		}
+		ApplyGyroRotation();
+		ApplyCalibration();
+		transform.rotation = Quaternion.Slerp(transform.rotation, m_rawGyroRotation.rotation, m_smoothing);
+	}
+
+	private IEnumerator CalibrateYAngle()
+	{
+		m_tempSmoothing = m_smoothing;
+		m_smoothing = 1;
+		m_calibrationYAngle = m_appliedGyroYAngle - m_initialYAngle; 
+		yield return null;
+		m_smoothing = m_tempSmoothing;
+	}
+
+	private void ApplyGyroRotation()
+	{
+		m_rawGyroRotation.rotation = Input.gyro.attitude;
+        m_rawGyroRotation.Rotate(0f, 0f, 180f, Space.Self);
+        m_rawGyroRotation.Rotate(90f, 180f, 0f, Space.World);
+        m_appliedGyroYAngle = m_rawGyroRotation.eulerAngles.y;
+	}
+
+	private void ApplyCalibration()
+	{
+		m_rawGyroRotation.Rotate(0f, -m_calibrationYAngle, 0f, Space.World); 
+	}
+
+	public void SetEnabled(bool value)
+	{
+		enabled = true;
+		StartCoroutine(CalibrateYAngle());
+	}
+}
